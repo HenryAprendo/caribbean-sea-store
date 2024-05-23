c16c173727acc9693b0d87ab0dcd461147f58179
HenryAprendo <hjsmipuc@gmail.com>
2024-05-23 11:47:48 -0500

    fiet: Implementación de autorización basada en roles

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,9 @@ import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common'
 import { LoginUserDto } from '../user/dto/login-user.dto';
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth.guard';
+import { Roles } from '../core/decorator/roles.decorator';
+import { Role } from '../core/enum/roles.enum';
+import { RolesGuard } from './roles.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -15,7 +18,8 @@ export class AuthController {
         return this.authService.authenticatedUser(dto.email, dto.password);
     }
 
-    @UseGuards(AuthGuard)
+    @UseGuards(AuthGuard,RolesGuard)
+    @Roles(Role.Customer,Role.Admin)
     @Get('profile')
     getProfile(@Request() req: any){
         return req.user;
